746f4817a74ce3a5a26bb682252d484f9897fda4
robbyhalintar2 <67970287+robbyhalintar2@users.noreply.github.com>
2020-09-05 12:16:17 +0700

    First

--- cmds/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (bot, message, args) => {
-message.channel.send('Pong! `' + Math.floor(Math.round(bot.ping)) + '`ms')
-
-message.delete();
-
-}
-  module.exports.help = {
-    name: "ping"
-  }
